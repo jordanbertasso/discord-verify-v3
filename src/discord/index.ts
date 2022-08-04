c6c523fe193d6f0f39874f2e04b3aa0e2922cec3
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2022-08-04 17:13:34 +1000

    Allow hyphen in email and number only at the end

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -159,7 +159,7 @@ client.on('interactionCreate', async (interaction) => {
   const mqID = interaction.fields.getTextInputValue('idInput');
 
   const fullNameRegex = /^\w+ \w+(?: \w+)*$/;
-  const emailRegex = /^[a-z]+\.[a-z0-9]+@(students\.mq\.edu\.au|mq\.edu\.au)$/;
+  const emailRegex = /^[a-z-]+\.[a-z]+[0-9]*@(students\.mq\.edu\.au|mq\.edu\.au)$/;
   const idRegex = /^(mq|MQ)?[0-9]{8,12}$/;
 
   if (!fullNameRegex.test(fullName)) {
